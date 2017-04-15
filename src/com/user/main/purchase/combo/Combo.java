09ae65c6724de07009410d082eda1b7bc393b5f0
aahyoung <zziibess@naver.com>
2017-04-15 19:00:45 +0900

    연실이 수행할 패널들! 주석으로 대략 설명 해뒀음

---
+++ src/com/user/main/purchase/combo/Combo.java
@@ -0,0 +1,8 @@
+package com.user.main.purchase.combo;
+
+public class Combo { //Combo는 Jpanel을 상속받음
+	//라벨 2개, 각각 콤보이름, 가격을 출력
+	//이미지, 이미지는 Combo테이블에 적혀있는 이미지 파일 이름을 가지고 서버에서 가져옴 
+	//(아직 서버가 없으므로 res_manager에 콤보이미지를 넣어두고 가져오면 됨!)
+	//이 3가지 컴포넌트를 여기에 add하고 ComboChoiceScreen에 부착
+}
--- src/com/user/main/purchase/combo/ComboChoiceScreen.java
+++ src/com/user/main/purchase/combo/ComboChoiceScreen.java
@@ -4,7 +4,11 @@ import com.user.frame.ScreenFrame;
 import com.user.main.ClientMain;
 
 public class ComboChoiceScreen extends ScreenFrame{
+	
+	
 	public ComboChoiceScreen(ClientMain main) {
 		super(main);
+		
+		//Combo라는 JPanel을 상속받은 객체를 db에 저장된 콤보리스트와 콤보를 조인하여 추출
 	}
 }
